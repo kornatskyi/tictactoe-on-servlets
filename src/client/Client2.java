f0790c7931d882540677baad9d01e2846883a09f
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-05-02 18:40:48 -0700

    Two players version of TicTacToe game

---
+++ src/client/Client2.java
@@ -0,0 +1,44 @@
+package client;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Client2 {
+    public static void main(String[] args) {
+        final int port = 5000;
+        final String host = "localhost";
+        Scanner scanner = new Scanner(System.in);
+        DataOutputStream out = null;
+        DataInputStream in = null;
+        DataInputStream stdIn = new DataInputStream(System.in);
+        try {
+            Socket clientSocket = new Socket(host, port);
+            System.out.println("Socket created.");
+
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            in = new DataInputStream(clientSocket.getInputStream());
+
+            try {
+                String str = "";
+                while (!(str = in.readUTF()).equals("Over")) {
+
+                    if (str.equals("n")) {
+                        out.writeUTF(scanner.nextLine());
+                    } else {
+                        System.out.println(str);
+                    }
+                }
+            } catch (EOFException e) {
+                System.out.println("Eof");
+            }
+
+
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
